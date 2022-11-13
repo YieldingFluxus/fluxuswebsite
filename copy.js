7fba39d0bfdbc9c52f96f0d19e2e53c91f947f3e
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-11-13 01:03:26 +0100

    fix: :bug: make sure /new/ works even if / is the route

--- copy.js
+++ copy.js
@@ -2,6 +2,6 @@ import * as fse from 'fs-extra'
 import * as fs from 'fs';
 const to = process.argv.pop();
 const from = process.argv.pop();
-console.log('cp',from,'to',to);
-fse.ensureFileSync(to)
-fs.copyFileSync(from,to)
+console.log('cp', from, 'to', to);
+fse['ensure'+(fs.statSync(from).isDirectory()?'Dir':'File')+'Sync'](to)
+fse.copy(from,to)
